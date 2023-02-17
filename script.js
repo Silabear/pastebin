53023b949693dc4d711766179c5ee6a35550628e
Silabear <56885288+Silabear@users.noreply.github.com>
2023-02-17 18:38:38 +0000

    Chekc if value exist

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const THISURL = "https://paste.silabear.xyz/";
 
 async function processPaste() {
+  // Show loading icon
   document.getElementById("press").innerHTML =
     '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+  
+  // Parse data
   let name = document.getElementById("nameinput").value;
-  if (name === undefined) {
+  if (name === "") {
     name = "Untitled Paste";
   }
   let value = document.getElementById("valueinput").value;
+  
+  // Check if value actually exists
+  if(value === "") {
+    document.getElementById("press").style = "width: 350px";
+    return document.getElementById("press").innerHTML = "error: paste content can't be empty!";
+  }
+  
   let data = {
     name: name,
     value: value,
@@ -27,21 +37,25 @@ async function processPaste() {
 }
 
 async function pageLoad() {
+  // Get ID from URL
   const myUrl = new URL(window.location.toLocaleString()).searchParams;
   let id = myUrl.get("p");
-  if (id != undefined) {
+  
+  if (id != undefined) { // Run code if there actually is an ID
     let divbox = document.getElementById("content");
-    divbox.innerHTML = "";
-    var res = await fetch("https://paste.silabear.workers.dev/" + id, {
+    divbox.innerHTML = ""; // Clear content box
+    
+    var res = await fetch("https://paste.silabear.workers.dev/" + id, { // Get the paste from the server
       method: "GET",
     }).catch((err) => alert(err));
+    
     let t = await res.text();
+    
+    // Get data and show data
     let o = JSON.parse(t);
-    // alert(`<div class="title">${o.name}</div><div class="text">${o.value}</div>`)
+    document.getElementById("desc").setAttribute("content", o.name);
     divbox.innerHTML = `<div class="title">${o.name}</div><div class="text">${o.value.replace(/\n/g,"<br />")}</div>`;
-    // alert("should be didded!")
   }
 }
 
 pageLoad();
-// plz deploy my code
